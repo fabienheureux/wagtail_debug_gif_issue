1472937e401a42a3658263dba70cc4ef0b6141db
Fabien Le Frapper <contact@fabienlefrapper.me>
2022-07-27 10:31:17 +0200

    initial commit

---
+++ debug_gif/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-txzt-+g=+4^0w)g#21um)=g-&nbk(%!&uzw$_6%vy8y-4*0s4d"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
